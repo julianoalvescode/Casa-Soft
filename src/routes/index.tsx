499f276fc8a30a39a1fdbe77c4862948c1e59ba9
Juliano Alves <contato.julianoalvess@gmail.com>
2020-08-29 01:11:23 -0300

    feat: Add

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+const Routes: React.FC = () => {
+    return (
+        <>
+            <Switch>
+                <Route path="/" exact />
+            </Switch>
+        </>
+    )
+}
+
+export default Routes
